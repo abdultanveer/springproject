a4d7e10bdf8f11244ab18dedadab28787949498f
abdul <abdul.tanveer@gmail.com>
2021-04-19 14:35:24 +0530

    vehicle test

--- FirstSpringProject/src/basics/VehicleTest.java
+++ FirstSpringProject/src/basics/VehicleTest.java
@@ -1,10 +1,19 @@
 package basics;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 public class VehicleTest {
 	public static void main(String[] args) {
-		Vehicle vehicle = new Bike();
-				//new Car();
+		Resource resource = new ClassPathResource("spring.xml");
+		BeanFactory beanFactory = new XmlBeanFactory(resource);
+		
+		Vehicle vehicle = (Vehicle) beanFactory.getBean("vehicle");
 		vehicle.drive();
+				//new Bike();
+				//new Car();
 		
 		/*
 		 * Bike bike = new Bike(); bike.drive();
