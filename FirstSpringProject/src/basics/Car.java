0e01e22f71ff0de247348d3458fbca701131c791
abdul <abdul.tanveer@gmail.com>
2021-04-19 15:09:55 +0530

    application context

--- FirstSpringProject/src/basics/Car.java
+++ FirstSpringProject/src/basics/Car.java
@@ -1,6 +1,7 @@
 package basics;
 
 public class Car implements Vehicle{
+	Steering steering;
 	
 	public void drive() {
 		System.out.println("car is driven");
--- FirstSpringProject/src/basics/Test.java
+++ FirstSpringProject/src/basics/Test.java
@@ -2,18 +2,33 @@ package basics;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+
+import constructor.injection.Employee;
 
 
 public class Test {
 	public static void main(String[] args) {
 		//Student abdul = new Student();
 		
-		Resource resource = new ClassPathResource("applicationContext.xml");
-		BeanFactory beanFactory = new XmlBeanFactory(resource);
-		Student abdul =	(Student) beanFactory.getBean("studentbean"); //bean is a pojo class
-		abdul.displayInfo();
+		 Resource r=new ClassPathResource("applicationContext.xml");  
+	        BeanFactory factory=new XmlBeanFactory(r);  
+	          
+	        Employee s=(Employee)factory.getBean("employee");  
+	        s.show(); 
+		
+		/*
+		 * Resource resource = new ClassPathResource("applicationContext.xml");
+		 * ApplicationContext context = new
+		 * ClassPathXmlApplicationContext("applicationContext.xml");
+		 * 
+		 * //BeanFactory beanFactory = new XmlBeanFactory(resource); Student abdul =
+		 * (Student) context.getBean("studentbean"); //bean is a pojo class
+		 * abdul.displayInfo();
+		 */
 	}
 
 }
